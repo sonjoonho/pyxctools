89727fec56f1937fad7ecc51c9f4a3b721347898
sonjoonho <json0820@gmail.com>
2018-12-22 19:48:38 +0000

    Added basic CLI.

---
+++ pyxctools.py
@@ -0,0 +1,17 @@
+import argparse
+from pyxctools.xenocanto import XenoCanto
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--query", help="Basic search terms.", required=True)
+
+    # TODO Add advanced terms...
+
+    parser.add_argument("-p", "--page", type=int, default=None, help="Page number.", required=False)
+    parser.add_argument("-d", "--dir", type=str, default="sounds", help="Directory to download to.", required=False)
+
+    args = parser.parse_args()
+
+    xc = XenoCanto()
+
+    xc.download_files(search_terms=args.query, page=args.page, dir=args.dir)
